b6f48644403f67e575565d33aeaf28e5e4f1f522
XxMitakaXx <mitakavarbanov05@gmail.com>
2024-05-31 12:52:47 +0300

    Initial commit

---
+++ src/main/java/org/example/finalprojectmyshop/FinalProjectMyShopApplication.java
@@ -0,0 +1,13 @@
+package org.example.finalprojectmyshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectMyShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectMyShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/finalprojectmyshop/FinalProjectMyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.finalprojectmyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectMyShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
