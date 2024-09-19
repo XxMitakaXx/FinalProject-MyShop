3755e05782f21ca596597532307a4314436a8482
XxMitakaXx <mitakavarbanov05@gmail.com>
2024-09-19 15:45:00 +0300

    Fixing issues

--- src/main/java/org/example/finalprojectmyshop/user/config/SecurityConfig.java
+++ src/main/java/org/example/finalprojectmyshop/user/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
                         authorizeRequest ->
                                 authorizeRequest
                                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                                        .requestMatchers("/", "/users/login", "/users/login-error","/users/register", "/product-details/{id}", "/find-products-by-name", "find-products-by-category/{categoryName}", "/error").permitAll()
+                                        .requestMatchers("/", "/users/login", "/users/login-error","/users/register", "/product-details/{id}", "/find-products-by-name", "find-products-by-category/{categoryName}", "/error", "/users-register-1").permitAll()
                                         .anyRequest()
                                         .authenticated()
                 )
--- src/main/java/org/example/finalprojectmyshop/user/web/UserRegisterController.java
+++ src/main/java/org/example/finalprojectmyshop/user/web/UserRegisterController.java
@@ -3,10 +3,8 @@ package org.example.finalprojectmyshop.user.web;
 import jakarta.validation.Valid;
 import org.example.finalprojectmyshop.order.models.dtos.imports.SearchProductByNameDTO;
 import org.example.finalprojectmyshop.user.models.dtos.imports.UserRegisterDTO;
-import org.example.finalprojectmyshop.user.models.entities.enums.UserRole;
 import org.example.finalprojectmyshop.user.service.UserService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -37,9 +35,7 @@ public class UserRegisterController {
     }
 
     @GetMapping("/register")
-    public String viewRegister(Model model) {
-        model.addAttribute("roles", UserRole.values());
-
+    public String viewRegister() {
         return "register";
     }
 
@@ -56,14 +52,6 @@ public class UserRegisterController {
 
             return "redirect:/users/register";
         }
-//
-//        if (!data.getPassword().equals(data.getConfirmPassword())) {
-//            // TODO handle error
-//            redirectAttributes.addFlashAttribute("userRegisterDTO", data);
-//            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.userRegisterDTO", bindingResult);
-//
-//            return "redirect:/users/register";
-//        }
 
         this.userService.register(data);
 
